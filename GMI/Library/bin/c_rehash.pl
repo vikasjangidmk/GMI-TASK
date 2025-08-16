0c898c1c93de5dd60a5623c973e4c743d743a6a9
vikasjangidmk <130329907+vikasjangidmk@users.noreply.github.com>
2025-08-16 11:15:21 +0530

    updated

--- GMI/Library/bin/c_rehash.pl
+++ GMI/Library/bin/c_rehash.pl
@@ -13,7 +13,7 @@
 # and add symbolic links to their hash values.
 
 my $dir = "C:\\Program Files\\Common Files\\ssl";
-my $prefix = "C:/Users/vikas/OneDrive/Desktop/GMI-TASK/GMI\\Library";
+my $prefix = "C:/Users/vikas/OneDrive/Desktop/GMI-TASK/gmi\\Library";
 
 my $errorcount = 0;
 my $openssl = $ENV{OPENSSL} || "openssl";
